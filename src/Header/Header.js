b267bdf3ad909c5ec21019c2eb90c6f899181172
emanuel <41143874+emanuelrouse@users.noreply.github.com>
2019-05-22 16:46:24 -0400

    Add Header Component

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>ELF Computing</h1>
+      <h3>Laptops</h3>
+      <h5>Customize your laptop</h5>
+    </header>
+  );
+}
