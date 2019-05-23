7365eb502be82a3a4f193a19487146273d9f814d
Christopher Williams <cw@chriswillia.ms>
2019-05-23 12:43:35 -0500

    Breaks out header into new component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './Header/Header'
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -75,11 +77,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing</h1>
-          <h3>Laptops</h3>
-          <h5>Customize your laptop</h5>  
-        </header>      
+         <Header />    
         <main>
           <section className="main__form">
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+            <h1>ELF Computing</h1>
+            <h3>Laptops</h3>
+            <h5>Customize your laptop</h5>  
+            </header> 
+        );
+    }
+}
